9c1a36f8cf50f9d6bcf0334c698f26f5fc231670
Benoit VALLON <benoitvallon@gmail.com>
2014-11-05 13:06:44 +0100

    Adds the keyboard navigation

--- app-ajax.js
+++ app-ajax.js
@@ -16,8 +16,45 @@ template.pages = [
 ];
 
 template.addEventListener('template-bound', function(e) {
+  scaffold = document.querySelector('#scaffold');
+  ajax = document.querySelector('#ajax');
+  pages = document.querySelector('#pages');
+  var keys = document.querySelector('#keys');
+
+  // Allow selecting pages by num keypad. Dynamically add
+  // [1, template.pages.length] to key mappings.
+  var keysToAdd = Array.apply(null, template.pages).map(function(x, i) {
+    return i + 1;
+  }).reduce(function(x, y) {
+    return x + ' ' + y;
+  });
+  keys.keys += ' ' + keysToAdd;
+
   // Use URL hash for initial route. Otherwise, use the first page.
   this.route = this.route || DEFAULT_ROUTE;
 });
+
+template.keyHandler = function(e, detail, sender) {
+  // Select page by num key.
+  var num = parseInt(detail.key);
+  if (!isNaN(num) && num <= this.pages.length) {
+    pages.selectIndex(num - 1);
+    return;
+  }
+
+  switch (detail.key) {
+    case 'left':
+    case 'up':
+      pages.selectPrevious();
+      break;
+    case 'right':
+    case 'down':
+      pages.selectNext();
+      break;
+    case 'space':
+      detail.shift ? pages.selectPrevious() : pages.selectNext();
+      break;
+  }
+};
 
 })();
